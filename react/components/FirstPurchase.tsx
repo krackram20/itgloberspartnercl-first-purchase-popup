95575437d72cbddd5dc17addf779c4778a8b0b86
Kevin <kevin.511@outlook.com>
2022-07-01 23:54:17 -0500

    impplementacion del popup en el primer render d la pagina

--- react/components/FirstPurchase.tsx
+++ react/components/FirstPurchase.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import './styles.css'
@@ -30,7 +30,14 @@ const FirstPurchase = () => {
   }
 
 
+  // closing button
 
+  const [openModal, setOpenModal] = useState(false)
+
+  useEffect(() => {
+
+    setTimeout(()=> {setOpenModal(true)}, 5000)
+  }, [])
 
   // const arr = emailsList.users.data.map((obj:any)=> {return obj.id})
 
@@ -38,16 +45,18 @@ const FirstPurchase = () => {
 
 
 
-
+ const [displaymessage, setDisplayMessage] = useState("")
 
  const validateEmail = () => {
   if (emails.includes(email)) {
-    console.log('email is already registeredalllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll')}
-  else {console.log('success');
+
+    setDisplayMessage('use another one')}
+  else {
+     setDisplayMessage('Success')
   }
  }
 
-  const contentStyle = { background: '#000' };
+  const contentStyle = { background: 'rgba(255,255,255,0.5)' };
   const overlayStyle = { background: 'rgba(0,0,0,0.5)' };
   const arrowStyle = { color: '#000' }
 
@@ -58,31 +67,44 @@ const FirstPurchase = () => {
   }
 
   return <div>
-
-    Fourth Component
-    <Popup trigger={<button> Trigger</button>} position="right center"
+    <Popup open = {openModal} position="right center"
     {...{   contentStyle, overlayStyle, arrowStyle }}
     modal>
+    <div
+      style={{
+        height: "200px",
+        width: "300px",
+        borderRadius: "10px"
+      }}>
+
+     <button onClick={()=>{ setOpenModal(false) }}
+     style= {{
+
+     }}>
+      X
+      </button>
     <form onSubmit={onSubmit}
 
-    style={{
-      border: "1px solid black"
-    }}>
+      style={{
+       border: "1px solid black"
+      }}>
 
+<div>
+      <input onChange={(e) => {setEmail(e.target.value)}}
+        type="email"
+        name="email"
+        placeholder="Email"
+        pattern="[A-Za-z0-9._+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
+        title="No se permiten caracteres especiales"
+        required
+      />
+    </div>
     <div>
-          <input onChange={(e) => {setEmail(e.target.value)}}
-            type="email"
-            name="email"
-            placeholder="Email"
-            pattern="[A-Za-z0-9._+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
-            title="No se permiten caracteres especiales"
-            required
-          />
-        </div>
-        <div>
-          <button >Submit Contact</button>
-        </div>
-    </form>
+      <button >Submit Contact</button>
+    </div>
+</form>
+<p>{displaymessage}</p>
+    </div>
     </Popup>
   </div>
 
